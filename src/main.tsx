3ff3f9f78615a05ac409c3a70306a76296ef4ac2
CAWilson94 <charlotteawilson.94@gmail.com>
2024-04-01 16:39:22 +0100

    data coming back for workitemlog

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const client = new ApolloClient({
+  uri: "http://localhost:9090/graphql",
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query MyQuery($id: ID!) {
+        WorkItemLogById(id: $id) {
+          id
+          date
+          department
+          actualLengthOfTime
+          author
+          estimatedLengthOfTime
+          workType
+        }
+      }
+    `,
+    variables: { 
+      id: "01"
+    }
+  })
+  .then((result) => console.log(result));
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
